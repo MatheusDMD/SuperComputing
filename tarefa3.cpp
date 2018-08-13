781822fd971fc95ae371afb6e1d691e35327a261
MatheusDMD <matheusdmd@al.insper.edu.br>
2018-08-13 10:41:42 -0300

    SIMD-1: Updating User print interface

--- tarefa3.cpp
+++ tarefa3.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <stdio.h>
 #include "funcs.cpp"
 #define size_of_array 100000000
 #define size_of_times 20
@@ -17,6 +18,8 @@ int main(){
     double *b = new double [size_of_array];
     default_random_engine generator;
     uniform_real_distribution<double> distribution(0,100);
+    cout << "SIMD Report Program\n";
+    cout << "Initializing Random Array\n\n";
     for(unsigned long i = 0; i < n; i++){
         double value = distribution(generator); 
         a[i] = value;
@@ -29,9 +32,11 @@ int main(){
     double x;
 
     //inner_prod
+    cout << "Inner Product\n"; 
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 1000; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             inner_prod(a, b, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -44,31 +49,30 @@ int main(){
             durations_array[t][i] = durations[i];
             cout << durations[i];
             cout << ',';
-            
         }
-            cout << '\n';
+        cout << '\n';
     }
     for(int t = 0; t < size_of_times; t++){
         durations[t] = 0;
         for(int h = 0; h < size_of_times; h++){
             durations[t] += durations_array[h][t];
-
         }
         durations[t] /= size_of_times;
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nINNER PRODUCT:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
 
     //sum_positive
+    cout << "Sum Positive\n";
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 10; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             sum_positive(a, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -93,20 +97,21 @@ int main(){
         }
         durations[t] /= size_of_times;
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nSUM POSITIVE:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
     index = 0;
 
     //sqrt_element
+    cout << "SQRT Element\n";
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 10; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             sqrt_element(a, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -131,20 +136,21 @@ int main(){
         }
         durations[t] /= size_of_times;
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nSQRT ELEMENT:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
     index = 0;
 
     //exp_element
+    cout << "Exp Element\n";
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 10; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             exp_element(a, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -169,20 +175,21 @@ int main(){
         }
         durations[t] /= size_of_times;
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nEXP ELEMENT:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
     index = 0;
 
     //log_element
+    cout << "Log Element\n";
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 10; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             log_element(a, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -207,20 +214,21 @@ int main(){
         }
         durations[t] /= size_of_times;
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nLOG ELEMENT:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
     index = 0;
 
     //gauss
+    cout << "Gauss Element\n";
+    cout << "Calculating Mean\n";
     for(int t = 0; t < size_of_times; t++){
         index = 0;
-        for(unsigned long s = 10; s <= size_of_array; s = s*10){
+        for(unsigned long s = 100000; s <= size_of_array; s = s*10){
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
             gauss(a,b, s);
             high_resolution_clock::time_point t2 = high_resolution_clock::now();   
@@ -246,14 +254,13 @@ int main(){
         durations[t] /= size_of_times;
     }
     
-    cout << '\n';
-    cout << '\n';
+    cout << "\nGAUSS ELEMENT:\n";
     for(int i = 0; i < index; i++){
         cout << durations[i];
         cout << ',';
     }
-    cout << '\n';
-    cout << '\n';
+    cout << "\nPaste values in the Google Sheets at: http://bit.ly/2P3IcCI ";
+    cout << "\n\n";
 
     return 0;
 }
